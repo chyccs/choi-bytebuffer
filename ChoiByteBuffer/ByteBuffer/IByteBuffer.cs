f563e9e32d11dfcf6c1abb9f51eac6268aa53667
chyccs <chyccs@neolab.net>
2021-03-19 11:38:43 +0900

    init

---
+++ ChoiByteBuffer/ByteBuffer/IByteBuffer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Choi.ByteBuffer
+{
+    public delegate byte[] EscapeDelegate(byte input);
+
+    public interface IByteBuffer : IDisposable
+    {
+        EscapeDelegate EscapeDelegate { get; set; }
+
+        int PositionToWrite { get; }
+
+        int PositionToRead { get; }
+
+        int Size { get; }
+
+        void PutBytes(params byte[] inputs);
+
+        byte[] GetBytes(int offset, int size);
+
+        IByteBuffer Put(params byte[] b);
+
+        IByteBuffer Put(byte[] b, int length);
+
+        IByteBuffer PutNull(int length);
+
+        IByteBuffer Put(short input);
+
+        IByteBuffer Put(ushort input);
+
+        IByteBuffer Put(int input);
+
+        IByteBuffer Put(uint input);
+
+        IByteBuffer Put(long input);
+
+        IByteBuffer Put(ulong input);
+
+        IByteBuffer Put(float input);
+
+        byte[] Get(int length);
+
+        T Get<T>();
+
+        T Get<T>(int length);
+
+        byte GetChecksum(int length);
+    }
+}
---
+++ ChoiByteBuffer/ByteBuffer/InMemoryByteBuffer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Choi.ByteBuffer
+{
+    public class InMemoryByteBuffer : DefaultByteBuffer
+    {
+        private List<byte> data = new List<byte>();
+
+        public override void Dispose()
+        {
+            data.Clear();
+            data = null;
+            GC.SuppressFinalize(this);
+        }
+
+        public override int Size
+        {
+            get { return data.Count; }
+        }
+
+        public InMemoryByteBuffer(byte[] data = null, EscapeDelegate escape = null)
+        {
+            EscapeDelegate = escape;
+            if (data != null && data.Length > 0)
+                this.data.AddRange(data.ToList());
+        }
+
+        public override void PutBytes(params byte[] input)
+        {
+            foreach (var b in input)
+                data.Add(b);
+            PositionToWrite += input.Length;
+        }
+
+        public override byte[] GetBytes(int offset, int length)
+        {
+            return data.Skip(offset).Take(length).ToArray();
+        }
+
+        public byte[] ToArray()
+        {
+            return data.ToArray();
+        }
+    }
+}
